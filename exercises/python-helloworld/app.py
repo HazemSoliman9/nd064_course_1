e6b1a551f5dfb233854fdc6a5e40398624918d23
HazemSoliman9 <hazemsamehhh@gmail.com>
2022-10-28 14:12:38 +0200

    added status route

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,16 @@
+import http
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+@app.route("/status")
+def status():
+    http.status_code = 200
+    return {
+        "result": "OK - healthy"
+    }
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
